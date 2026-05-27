e8846d62ebb22d628ab962774dd3ab97b8ae036c
Yurii Postoronka <yurii.postoronka@axisbits.com>
2026-05-27 13:44:45 +0300

    fix: use relative imports for layouts in router to fix alias resolution in dev mode

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DefaultLayout     from "../layouts/DefaultLayout.vue";
-import AdminLayout from "@/layouts/AdminLayout.vue";
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+import AdminLayout from '../layouts/AdminLayout.vue'
 
 const routers = [
 
